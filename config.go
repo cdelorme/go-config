aadb590ac1386a484855d58176b4ac28d4218977
Casey DeLorme <cdelorme@gmail.com>
2014-10-20 02:51:13 -0400

    eliminated all complexity in favor of simple functions

--- config.go
+++ config.go
@@ -1,75 +1,60 @@
 package config
 
-/**
- * Configuration abstraction
- *
- * Author: Casey Delow <cdelorme@gmail.com>
- * Date: 2014-8-10
- */
-
 import (
 	"encoding/json"
 	"errors"
 	"os"
+	"os/user"
+	"path"
 )
 
-type Config struct {
-	Settings
-	Preferences
-	File string
+func Load(file string) (map[string]interface{}, error) {
+	conf := make(map[string]interface{})
+	files := []string{file}
+	appName := path.Base(os.Args[0])
+	usr, err := user.Current()
+	if err == nil {
+		files = append(files, usr.HomeDir+"/."+appName)
+	}
+	files = append(files, "/etc/"+appName)
+	for _, f := range files {
+		openFile, fileErr := os.Open(f)
+		defer openFile.Close()
+		if fileErr == nil {
+			decoder := json.NewDecoder(openFile)
+			jsonErr := decoder.Decode(&conf)
+			if jsonErr != nil {
+				return conf, jsonErr
+			}
+		}
+	}
+	return conf, nil
 }
 
-func (config *Config) Load() error {
-	defer config.Override()
-
-	if config.File == "" {
-		return errors.New("No file supplied...")
+func Save(file string, data *map[string]interface{}) error {
+	if file == "" {
+		usr, err := user.Current()
+		if err != nil {
+			return errors.New("No supplied or accessible files for save operation")
+		}
+		file = usr.HomeDir + "/." + path.Base(os.Args[0])
 	}
 
-	openFile, fileErr := os.Open(config.File)
+	openFile, fileErr := os.Create(file)
 	defer openFile.Close()
 	if fileErr != nil {
 		return fileErr
 	}
 
-	decoder := json.NewDecoder(openFile)
-	jsonErr := decoder.Decode(&config.Data)
+	js, jsonErr := json.MarshalIndent(data, "", "    ")
 	if jsonErr != nil {
 		return jsonErr
 	}
 
-	return nil
-}
-
-func (config *Config) Save() error {
-	if config.File == "" {
-		return errors.New("No file supplied...")
-	}
-
-	openFile, fileErr := os.Create(config.File)
-	defer openFile.Close()
-	if fileErr != nil {
-		return fileErr
-	}
-
-	data, jsonErr := json.MarshalIndent(&config.Data, "", "    ")
-	if jsonErr != nil {
-		return jsonErr
-	}
-
-	_, writeErr := openFile.Write(data)
+	_, writeErr := openFile.Write(js)
 	if writeErr != nil {
 		return writeErr
 	}
 
 	return nil
-}
-
-func (config *Config) Override() {
-	config.Parse()
-	for _, flag := range config.Options {
-		if *flag.Value != "" {
-			config.Set(*flag.Value, flag.Keys...)
-		}
-	}
 }
